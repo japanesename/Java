788eecf72043d9ea7561d5a53ea7bd6e7d0e86d5
japanesename <lolflip54@gmail.com>
2021-01-07 09:29:29 -0800

    Add files via upload

---
+++ Doubt1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+	allObjectsQuantity appleQuantity = new allObjectsQuantity(8);
+	appleQuantity.setColour("Red");
+	
+	allObjectsQuantity orangeQuantity = new allObjectsQuantity(4);
+	orangesQuantity.setColour("Orange");
+	
+	allObjectsQuantity cucumberQuantity = new allObjectsQuantity(6);
+	apples.setColour("Light Green");
+	
+	allObjectsQuantity capsicumQuantity = new allObjectsQuantity(2);
+	apples.setColour("Green");
+	
+	System.out.println("Number of apples: " + appleQuantity.getQuantity());
+}
---
+++ Doubt1/src/fruitsAndVegetables.java
@@ -0,0 +1,15 @@
+
+
+public class fruitsAndVegetables {
+	public int Quantity;
+	private String Colour;
+	public static void main(String[] args) {
+		
+		 allObjectsQuantity(String quantityCount) {
+			this.Quantity = quantityCount;
+		   }
+		String getQuantity() {
+			return Quantity
+		}
+	}
+}
