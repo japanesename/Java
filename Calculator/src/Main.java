e958a0d5dc3767d31c1679f4f4ed50d233cffb7c
japanesename <lolflip54@gmail.com>
2021-01-12 07:36:19 -0800

    Add files via upload

---
+++ Calculator/src/Main.java
@@ -0,0 +1,24 @@
+import calc.Calculator;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		int addition = calc.add(1, 3);
+		System.out.println(addition + " is the sum of the two numbers given");
+		
+		int subtraction = calc.diff(6, 2);
+		System.out.println(subtraction + " is the difference of the two numbers given");
+		
+		int multiply = calc.multiply(7, 3);
+		System.out.println(multiply + " is the product of the two numbers given");
+		
+		int division = calc.division(8, 4);
+		System.out.println(division + " is the division of the two numbers given");
+		/* Increment and Decrement */
+		System.out.println(calc.increment(7) + " is the increment of the number given");
+
+		System.out.println(calc.decrement(9) + " is the decrement of the number given");
+	}
+}
---
+++ Calculator/src/calc/Calculator.java
@@ -0,0 +1,45 @@
+package calc;
+
+public class Calculator {
+	
+	/* Addition Code*/
+	public int add(int num1, int num2) {
+		int sum = num1 + num2;
+				return sum;
+	}
+	/* Addition Code Ends Here*/
+	
+	/* Subtraction Code*/
+	public int diff(int num1, int num2) {
+		int diff = num1 - num2;
+				return diff;
+	}
+	/* Subtraction Code Ends Here*/
+	
+	/* Multiplication Code*/
+	public int multiply(int num1, int num2) {
+		int multiply = num1 * num2;
+				return multiply;
+	}
+	/* Multiplication Code Ends Here*/
+	
+	/* Division Code*/
+	public int division(int num1, int num2) {
+		int division = num1 / num2;
+				return division;
+	}
+	/* Division Code Ends Here*/
+	
+	/* Increment Code*/
+	public int increment(int num1) {
+		int increment = num1++;
+				return increment;
+	}
+	/* Increment Code Ends Here*/
+	
+	/* Decrement Code*/
+	public int decrement(int num1) {
+		return num1--;
+	}
+	/* Decrement Code Ends Here*/
+}
