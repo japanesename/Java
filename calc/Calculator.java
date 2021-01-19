2cf919e41073a0d39d38d78c1f8631909549f1fe
japanesename <lolflip54@gmail.com>
2021-01-19 00:10:21 -0800

    Getting error

---
+++ calc/Calculator.java
@@ -0,0 +1,46 @@
+package calc;
+
+public class Calculator {
+	
+	/* Addition Code*/
+	public int add(int num1, int num2) {
+		int sum = num1 + num2;
+				return sum;
+	}
+	/* Addition Code Ends Here*/
+	
+	/* Subtraction Code*/
+	public int diff(int num1, int num2) {
+		int diff = num1 - num2;
+				return diff;
+	}
+	/* Subtraction Code Ends Here*/
+	
+	/* Multiplication Code*/
+	public int multiply(int num1, int num2) {
+		int multiply = num1 * num2;
+				return multiply;
+	}
+	/* Multiplication Code Ends Here*/
+	
+	/* Division Code*/
+	public int division(int num1, int num2) {
+		int division = num1 / num2;
+				return division;
+	}
+	/* Division Code Ends Here*/
+	
+	/* Increment Code*/
+	public int increment(int num1) {
+		num1++;
+		 return num1;
+	}
+	/* Increment Code Ends Here*/
+	
+	/* Decrement Code*/
+	public int decrement(int num1) {
+		 num1--;
+		 return num1;
+	}
+	/* Decrement Code Ends Here*/
+}
