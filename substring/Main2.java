c59fe49def3a2695fa9feb418e4b0c5a7e68cd88
japanesename <lolflip54@gmail.com>
2021-01-21 11:17:29 -0800

    Add files via upload

---
+++ substring/Main2.java
@@ -0,0 +1,11 @@
+package substring;
+
+public class Main2 {
+	public static void main(String args[]) {
+	      String hi = new String("Hi Nani");
+	      String hidden = new String("navina suman");
+	      System.out.println(hi);
+	      String sub = hidden.substring(0, 9);
+	      System.out.println("nanis first name is " + sub);
+	   }
+}
