78cbb216220e9e439e2cf09246002c2a7e050f05
japanesename <lolflip54@gmail.com>
2021-01-22 09:13:04 -0800

    Add files via upload

---
+++ Triangle/src/Triangle.java
@@ -0,0 +1,9 @@
+public class Triangle {
+
+  public static void main(String[] args) {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; ++i)
+    	System.out.print("* ");
+    }
+  }
---
+++ substring2/src/substring/Main2.java
@@ -0,0 +1,9 @@
+package substring;
+
+public class Main2 {
+	public static void main(String args[]) {
+		long odd = 123456789;
+		byte i = 1;
+		long print = odd.substring(++i)
+	   }
+}
