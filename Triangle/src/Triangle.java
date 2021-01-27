84f4481d5eabe3ca81131cde8f9780dbe145dc52
japanesename <lolflip54@gmail.com>
2021-01-27 00:58:48 -0800

    Add files via upload

--- Triangle/src/Triangle.java
+++ Triangle/src/Triangle.java
@@ -2,8 +2,22 @@ public class Triangle {
 
   public static void main(String[] args) {
     int rows = 5;
+    {
+    for(int k=0; k<rows; k++)   
+    {   
+    for(int j=0; j<=k; j++)   
+    {   
+    System.out.print("* ");   
+    }   
+    System.out.println();   
+    for (int i = rows; i >= 1; i--) {
+    	
+    	for(int j = i; j>= 1; j--) {
+    		System.out.print("* ");
+    	}
+    	
+    	System.out.println();
+    }
 
-    for (int i = 1; i <= rows; ++i)
-    	System.out.print("* ");
     }
   }
